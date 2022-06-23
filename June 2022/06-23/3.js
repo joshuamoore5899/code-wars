a79108b2a3d016d44f9c8512ab8bb095fb5144be
joshuamoore5899 <joshuamoore5899@gmail.com>
2022-06-23 11:53:36 -0600

    leetcode 06-23

---
+++ June 2022/06-23/3.js
@@ -0,0 +1,46 @@
+// 942. DI String Match
+//
+// A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+//
+// s[i] == 'I' if perm[i] < perm[i + 1], and
+// s[i] == 'D' if perm[i] > perm[i + 1].
+// Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+//
+//
+//
+// Example 1:
+//
+// Input: s = "IDID"
+// Output: [0,4,1,3,2]
+// Example 2:
+//
+// Input: s = "III"
+// Output: [0,1,2,3]
+// Example 3:
+//
+// Input: s = "DDI"
+// Output: [3,2,0,1]
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 105
+// s[i] is either 'I' or 'D'.
+
+var diStringMatch = function(s) {
+    let end = s.length;
+    let start = 0;
+    let ans = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'I') {
+            ans.push(start);
+            start++;
+        }
+        else {
+            ans.push(end);
+            end--;
+        }
+    }
+    ans.push(start)
+    return ans;
+};

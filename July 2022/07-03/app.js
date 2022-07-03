43cc2f981502e2a3d5cbebb731e27be674fae124
Josh Moore <joshuamoore5899@gmail.com>
2022-07-03 11:48:36 -0600

    leetcode 07-03

--- July 2022/07-03/app.js
+++ July 2022/07-03/app.js
@@ -20,6 +20,7 @@
 // 1 <= words[i].length <= 100
 // words[i] consists of lowercase English letters.
 
+// with splice
 var commonChars = function(words) {
     let ans = [];
 
@@ -36,6 +37,30 @@ var commonChars = function(words) {
                     words[j] = words[j].split('');
                 }
                 words[j].splice(words[j].indexOf(temp), 1)
+            }
+        }
+        if (tracker) {
+            ans.push(temp)
+        }
+
+    }
+    return ans;
+};
+
+//with replace
+var commonChars = function(words) {
+    let ans = [];
+
+    for (let i = 0; i < words[0].length; i++) {
+        let temp = words[0][i];
+        let tracker = true;
+        for (let j = 1; j < words.length; j++) {
+            if (!words[j].includes(temp)) {
+                tracker = false;
+                break;
+            }
+            else {
+                words[j] = words[j].replace(temp, '')
             }
         }
         if (tracker) {

7c37f627f7c76a9dd10fc0a401f495552854615b
Josh Moore <joshuamoore5899@gmail.com>
2022-08-05 12:40:05 -0600

    codewars 08-04

---
+++ August 2022/08-04/1.js
@@ -0,0 +1,45 @@
+// Help Suzuki count his vegetables....
+//
+// Help Suzuki count his vegetables....
+//
+// Suzuki is the master monk of his monastery so it is up to him to ensure the kitchen is operating at full capacity to feed his students and the villagers that come for lunch on a daily basis.
+//
+// This week there was a problem with his deliveries and all the vegetables became mixed up. There are two important aspects of cooking in his kitchen, it must be done in harmony and nothing can be wasted. Since the monks are a record keeping people the first order of business is to sort the mixed up vegetables and then count them to ensure there is enough to feed all the students and villagers.
+//
+//
+// Return a list of tuples with the count of each vegetable in descending order. If there are any non vegetables mixed in discard them. If the count of two vegetables is the same sort in reverse alphabetical order (Z->A).
+//
+// (119, "pepper"),
+// (114, "carrot"),
+// (113, "turnip"),
+// (102, "onion"),
+// (101, "tofu"),
+// (100, "cabbage"),
+// (93, "mushroom"),
+// (90, "cucumber"),
+// (88, "potato"),
+// (80, "celery")
+
+function countVegetables(string){
+  let vegetables = ["cabbage", "carrot", "celery", "cucumber", "mushroom", "onion", "pepper", "potato", "tofu", "turnip"]
+  let list = string.split(" ");
+  let tracker = {};
+  for (let i = 0; i < list.length; i++) {
+    if (vegetables.indexOf(list[i]) != -1) {
+      tracker[list[i]] = tracker[list[i]] + 1 || 1;
+    }
+
+  }
+  let final = [];
+  for (const key in tracker) {
+    final.push([tracker[key], key])
+  }
+  return final.sort((a, b)=> {
+    if (a[0] != b[0]) {
+      return b[0] - a[0];
+    }
+    else {
+      return b[1].localeCompare(a[1])
+    }
+  })
+}

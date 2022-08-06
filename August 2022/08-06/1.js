cd2dd78f249fafeedea1935d598c04c846ef0af5
Josh Moore <joshuamoore5899@gmail.com>
2022-08-06 09:21:04 -0600

    codewars 08-06

---
+++ August 2022/08-06/1.js
@@ -0,0 +1,21 @@
+// The Feast of Many Beasts
+//
+// All of the animals are having a feast! Each animal is bringing one dish. There
+// is just one rule: the dish must start and end with the same letters as the animal's
+// name. For example, the great blue heron is bringing garlic naan and the chickadee is
+// bringing chocolate cake.
+//
+// Write a function feast that takes the animal's name and dish as arguments and returns
+// true or false to indicate whether the beast is allowed to bring the dish to the feast.
+//
+// Assume that beast and dish are always lowercase strings, and that each has at least two
+// letters. beast and dish may contain hyphens and spaces, but these will not appear at the
+// beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+  let firstBeast = beast[0];
+  let lastBeast = beast[beast.length - 1];
+  let firstDish = dish[0];
+  let lastDish = dish[dish.length - 1];
+  return firstBeast === firstDish && lastBeast === lastDish;
+}

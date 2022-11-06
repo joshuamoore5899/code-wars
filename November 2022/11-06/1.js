36e22f7c9e232f018ead97cb9386397b0b469c39
Josh Moore <joshuamoore5899@gmail.com>
2022-11-06 13:03:15 -0700

    codewars 11-06

---
+++ November 2022/11-06/1.js
@@ -0,0 +1,24 @@
+// Vowel one
+//
+// vowelOne
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+//
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+//
+// Examples:
+//
+// vowelOne( "abceios" ) // "1001110"
+//
+// vowelOne( "aeiou, abc" ) // "1111100100"
+
+function vowelOne(s){
+
+  return s.split('').map((x) => {
+    if ('aeiouAEIOU'.includes(x)) {
+      return '1'
+    }
+    else {
+      return '0'
+    }
+  }).join('');
+}

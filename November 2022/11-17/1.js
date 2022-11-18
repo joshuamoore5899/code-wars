f7009fdf7ddf6b136e9994f35d1a39cffd5371c9
Josh Moore <joshuamoore5899@gmail.com>
2022-11-17 21:27:35 -0700

    codewars 11-17

---
+++ November 2022/11-17/1.js
@@ -0,0 +1,5 @@
+//write a function that takes in an array and a target value and return the inde// of the target value
+
+function findIndex (targetValue, array) {
+	return array.indexOf(targetValue);
+}
